8c7a081ebc3ff16914a65ffa501e871df263ff31
ohnj8800 <ohnj8800@gmail.com>
2024-10-27 23:53:47 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <?php
-// 防止缓存
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
-
-// 连接数据库
-include "../inc/dbinfo.inc"; // 请确保 dbinfo.inc 包含你的数据库配置
-$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$database = mysqli_select_db($connection, DB_DATABASE);
-
-// 确保 EMPLOYEES 表存在
-VerifyEmployeesTable($connection, DB_DATABASE);
-
-// 检查是否有新的数据提交
-$employee_name = htmlentities($_POST['NAME']);
-$employee_address = htmlentities($_POST['ADDRESS']);
-if (strlen($employee_name) || strlen($employee_address)) {
-    AddEmployee($connection, $employee_name, $employee_address);
-}
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?>
-
 <!DOCTYPE html>
 <html lang="zh-Hant">
 <head>
@@ -39,43 +17,67 @@ if (strlen($employee_name) || strlen($employee_address)) {
         <h1>蘇聖元 自我介紹網站</h1>
     </header>
 
-    <!-- MySQL 数据输入表单 -->
     <div class="container">
-        <h2>輸入員工資料</h2>
-        <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-            <div class="form-group">
-                <label for="name">姓名：</label>
-                <input type="text" name="NAME" class="form-control" maxlength="45" required>
+        <div class="row">
+            <div class="col-md-12">
+                <?php include('menu.php'); ?>
+                <div class="jumbotron">
+                    <p>
+                        <?php include("get-index-meta-data.php"); ?>
+                        <hr />
+                        <?php include('get-cpu-load.php'); ?>
+                    </p>
+                </div>
             </div>
-            <div class="form-group">
-                <label for="address">地址：</label>
-                <input type="text" name="ADDRESS" class="form-control" maxlength="90" required>
-            </div>
-            <button type="submit" class="btn btn-primary">新增資料</button>
-        </form>
+        </div>
+    </div>
+    
+    <div class="container">
+        <img src="IMG_6477.gif"  class="profile-pic">
+        <div class="description">
+            <h2>個人簡介</h2>
+            <p>
+                我是資工三甲28號蘇聖元，平時喜歡玩遊戲和看動漫化或輕小說，最近玩很瘋的遊戲是Dark And Darker。
+            </p>
+            <p>
+                Email: 11111128@gm.nttu.edu.tw
+            </p>
+        </div>
     </div>
 
-    <!-- 数据库数据展示 -->
-    <div class="container mt-4">
-        <h2>員工列表</h2>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>姓名</th>
-                    <th>地址</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
-                while($query_data = mysqli_fetch_row($result)) {
-                    echo "<tr><td>$query_data[0]</td><td>$query_data[1]</td><td>$query_data[2]</td></tr>";
-                }
-                mysqli_free_result($result);
-                ?>
-            </tbody>
-        </table>
+    <div class="container">
+        <div class="description">
+            <h2>這是未來要抽的腳色</h2>
+            <img src="kisakilobby.jpg"  class="profile-pickisaki">
+        </div>
+        <img src="senseipr.png"  class="profile-picn">
+        <img src="kisakiface.png"  class="profile-picn">
+    </div>
+
+    <div class="container">
+        <div class="description">
+            <h2>推薦> 浮雲(feat. isui)-nao</h2>
+            <video width="560" height="315" controls>
+                <source src="cloud.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="description">
+            <h2>這我</h2>
+            <img src="po.jpg" class="profile-pic">
+            <h2>這我</h2>
+            <img src="usakifuck.png" class="profile-pic">
+        </div>
+        <img src="kokonagood.gif"  class="profile-picKOKO">
+    </div>
+
+    <div class="container">
+        <div class="description">
+            <h2>這我</h2>
+            <img src="me.jpg" class="profile-picself">
+        </div>
     </div>
 
     <footer>
@@ -87,37 +89,3 @@ if (strlen($employee_name) || strlen($employee_address)) {
     <script src="js/scripts.js"></script>
 </body>
 </html>
-
-<?php
-// 添加员工到数据库
-function AddEmployee($connection, $name, $address) {
-   $n = mysqli_real_escape_string($connection, $name);
-   $a = mysqli_real_escape_string($connection, $address);
-   $query = "INSERT INTO EMPLOYEES (NAME, ADDRESS) VALUES ('$n', '$a')";
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
-}
-
-// 确认表是否存在，如果不存在则创建
-function VerifyEmployeesTable($connection, $dbName) {
-  if(!TableExists("EMPLOYEES", $connection, $dbName)) {
-     $query = "CREATE TABLE EMPLOYEES (
-         ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-         NAME VARCHAR(45),
-         ADDRESS VARCHAR(90)
-       )";
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
-  }
-}
-
-// 检查表是否存在
-function TableExists($tableName, $connection, $dbName) {
-  $t = mysqli_real_escape_string($connection, $tableName);
-  $d = mysqli_real_escape_string($connection, $dbName);
-  $checktable = mysqli_query($connection, "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
-  if(mysqli_num_rows($checktable) > 0) return true;
-  return false;
-}
-
-// 关闭数据库连接
-mysqli_close($connection);
-?>
