d3f2c9e39afaac58ef3de6fbd4cc7a74746dfb1c
ohnj8800 <ohnj8800@gmail.com>
2024-10-28 00:30:20 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+// MySQL 数据库连接信息
+include "../inc/dbinfo.inc"; // 请确保此文件中包含正确的数据库连接信息
+
+// 建立与数据库的连接
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+if (mysqli_connect_errno()) {
+    echo "连接到 MySQL 失败: " . mysqli_connect_error();
+}
+
+// 选择数据库
+$database = mysqli_select_db($connection, DB_DATABASE);
+
+// 验证 EMPLOYEES 表是否存在
+VerifyEmployeesTable($connection, DB_DATABASE);
+
+// 如果表单填写了数据，添加数据到 EMPLOYEES 表
+$employee_name = htmlentities($_POST['NAME']);
+$employee_address = htmlentities($_POST['ADDRESS']);
+if (strlen($employee_name) || strlen($employee_address)) {
+    AddEmployee($connection, $employee_name, $employee_address);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="zh-Hant">
 <head>
@@ -31,9 +54,9 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
             </div>
         </div>
     </div>
-    
+
     <div class="container">
-        <img src="IMG_6477.gif"  class="profile-pic">
+        <img src="IMG_6477.gif" class="profile-pic">
         <div class="description">
             <h2>個人簡介</h2>
             <p>
@@ -48,10 +71,10 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     <div class="container">
         <div class="description">
             <h2>這是未來要抽的腳色</h2>
-            <img src="kisakilobby.jpg"  class="profile-pickisaki">
+            <img src="kisakilobby.jpg" class="profile-pickisaki">
         </div>
-        <img src="senseipr.png"  class="profile-picn">
-        <img src="kisakiface.png"  class="profile-picn">
+        <img src="senseipr.png" class="profile-picn">
+        <img src="kisakiface.png" class="profile-picn">
     </div>
 
     <div class="container">
@@ -70,7 +93,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
             <h2>這我</h2>
             <img src="usakifuck.png" class="profile-pic">
         </div>
-        <img src="kokonagood.gif"  class="profile-picKOKO">
+        <img src="kokonagood.gif" class="profile-picKOKO">
     </div>
 
     <div class="container">
@@ -83,6 +106,62 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
         </audio>
     </div>
 
+    <!-- 移到页面底部的资料输入功能和数据显示 -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>資料輸入</h2>
+                <!-- Input form -->
+                <form action="<?php echo $_SERVER['SCRIPT_NAME']; ?>" method="POST">
+                    <table border="0">
+                        <tr>
+                            <td>姓名</td>
+                            <td>地址</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" name="NAME" maxlength="45" size="30" />
+                            </td>
+                            <td>
+                                <input type="text" name="ADDRESS" maxlength="90" size="60" />
+                            </td>
+                            <td>
+                                <input type="submit" value="新增資料" />
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+
+                <!-- 顯示資料表 -->
+                <table border="1" cellpadding="2" cellspacing="2">
+                    <tr>
+                        <td>ID</td>
+                        <td>姓名</td>
+                        <td>地址</td>
+                    </tr>
+
+                    <?php
+                    $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+                    while($query_data = mysqli_fetch_row($result)) {
+                        echo "<tr>";
+                        echo "<td>", $query_data[0], "</td>",
+                             "<td>", $query_data[1], "</td>",
+                             "<td>", $query_data[2], "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+
+                </table>
+
+                <?php
+                // 清理
+                mysqli_free_result($result);
+                mysqli_close($connection);
+                ?>
+            </div>
+        </div>
+    </div>
+
     <footer>
         蘇聖元 自我介紹網站
     </footer>
@@ -92,3 +171,37 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
     <script src="js/scripts.js"></script>
 </body>
 </html>
+
+<?php
+/* 添加员工到表中 */
+function AddEmployee($connection, $name, $address) {
+    $n = mysqli_real_escape_string($connection, $name);
+    $a = mysqli_real_escape_string($connection, $address);
+    $query = "INSERT INTO EMPLOYEES (NAME, ADDRESS) VALUES ('$n', '$a');";
+    if(!mysqli_query($connection, $query)) echo("<p>添加员工数据时出错。</p>");
+}
+
+/* 检查表是否存在，并创建表 */
+function VerifyEmployeesTable($connection, $dbName) {
+    if(!TableExists("EMPLOYEES", $connection, $dbName)) {
+        $query = "CREATE TABLE EMPLOYEES (
+            ID int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            NAME VARCHAR(45),
+            ADDRESS VARCHAR(90)
+        )";
+
+        if(!mysqli_query($connection, $query)) echo("<p>创建表时出错。</p>");
+    }
+}
+
+/* 检查表的存在性 */
+function TableExists($tableName, $connection, $dbName) {
+    $t = mysqli_real_escape_string($connection, $tableName);
+    $d = mysqli_real_escape_string($connection, $dbName);
+    $checktable = mysqli_query($connection,
+        "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+
+    if(mysqli_num_rows($checktable) > 0) return true;
+    return false;
+}
+?>
